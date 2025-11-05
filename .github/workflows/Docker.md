5a760e8ba5c6052c9b9240bd64a420655f4aea83
filipwretner <filip.wretner@chasacademy.se>
2025-11-05 15:11:37 +0100

    Testing version tags

---
+++ .github/workflows/Docker.md
@@ -0,0 +1,5 @@
+Docker image taggning
+
+:latest - Pekar alltid på den senast byggda imagen. Bör inte användas i produktion.
+
+Exempelvis :v1.0.0 - Specifika versionstaggar som är verifiera för att vara stabila
--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Docker!');
+  res.send('Hello Docker from version 1.1!');
 });
 
 app.listen(PORT, () => {
